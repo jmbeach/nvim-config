b511c994e32d86eacf9a1c04a2efe4a25b335a59
jared.beach <jared.beach@capitalone.com>
2024-01-19 20:45:15 -0500

    remove ctrl + / remap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Remap ctrl + / to run gcc in normal mode
-vim.api.nvim_set_keymap("n", "<C-/>", ':execute "normal gcc"<CR>', { noremap = true, silent = true })
