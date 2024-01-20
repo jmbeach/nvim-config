9ba284d6891ad5eb50a2255d54d994927abae505
jmbeach <jaredbeachdesign@gmail.com>
2024-01-20 12:13:53 -0500

    autohotkey file extension

--- lua/config/file-associations.lua
+++ lua/config/file-associations.lua
@@ -1,9 +1,15 @@
--- Associate log files with log filetype
-for _, v in ipairs({ "BufRead", "BufNewFile", "FileType" }) do
-  vim.api.nvim_create_autocmd(v, {
-    pattern = "*.log",
-    callback = function()
-      vim.bo.filetype = "log"
-    end,
-  })
+local fileTypes = {
+  ["*.ah2"] = "autohotkey",
+  ["*.log"] = "log",
+}
+
+for extension, filetype in pairs(fileTypes) do
+  for _, v in ipairs({ "BufRead", "BufNewFile", "FileType" }) do
+    vim.api.nvim_create_autocmd(v, {
+      pattern = extension,
+      callback = function()
+        vim.bo.filetype = filetype
+      end,
+    })
+  end
 end
