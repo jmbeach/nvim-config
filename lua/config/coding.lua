236bd1060b8fa43295d2d0d5dc41092bf402ed68
jmbeach <jaredbeachdesign@gmail.com>
2024-01-20 22:10:23 -0500

    column select

--- lua/config/coding.lua
+++ lua/config/coding.lua
@@ -1,11 +1,14 @@
 -- multi-line cursor config
-
 vim.api.nvim_exec(
   [[
+" enable mouse for multi-line cursor
 let g:VM_mouse_mappings = 1
-nnoremap <A-LeftMouse> <Plug>(VM-Mouse-Cursor)
-xnoremap <A-LeftMouse> <Plug>(VM-Mouse-Cursor)
-inoremap <A-LeftMouse> <Plug>(VM-Mouse-Cursor)
+
+" Alt + Click adds multi-line cursor
+nmap <M-LeftMouse> <Plug>(VM-Mouse-Cursor)
+
+" \ \ Click does column select
+nmap \\<LeftMouse> <Plug>(VM-Mouse-Column)
 ]],
   false
 )
