5c266cc1edfbf65738f7cfe45bd872d2a085508e
jmbeach <jaredbeachdesign@gmail.com>
2024-02-28 11:22:11 -0500

    copy-paste

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,9 +14,11 @@ end
 vim.keymap.set("n", "<M-/>", lazyterm, { desc = "Terminal (root dir)" })
 vim.keymap.set("t", "<M-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
-
 -- Map leader f p to print full file path
 vim.keymap.set("n", "<leader>fp", ":lua print(vim.api.nvim_buf_get_name(0))<cr>", { desc = "Print full file path" })
 
 -- setup telescope live grep args
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+
+vim.keymap.set("v", "<leader>cc", '"y+', { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<leader>p", '"p+', { desc = "Paste from clipboard" })
