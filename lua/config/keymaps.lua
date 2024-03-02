0490c7b838de3243b40a9b7ca5c4a2cbaaea0aa0
jared.beach <jared.beach@capitalone.com>
2024-03-02 12:43:08 -0500

    copy / paste keymaps etc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,11 +14,21 @@ end
 vim.keymap.set("n", "<M-/>", lazyterm, { desc = "Terminal (root dir)" })
 vim.keymap.set("t", "<M-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local is_tmux = os.getenv("TMUX") ~= nil
+
+-- Fix some screwy stuff on mac in tmux
+if is_mac and is_tmux then
+  -- Ctrl + - seems to be mapped to ctrl + _
+  -- Overriding that to go back in jump list instead of opening terminal
+  vim.keymap.set("n", "<C-_>", "<C-o>", { desc = "Go back in jump list" })
+end
+
 -- Map leader f p to print full file path
 vim.keymap.set("n", "<leader>fp", ":lua print(vim.api.nvim_buf_get_name(0))<cr>", { desc = "Print full file path" })
 
 -- setup telescope live grep args
-vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+vim.keymap.set("n", "<leader>/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
-vim.keymap.set("v", "<leader>cc", '"y+', { desc = "Copy to clipboard" })
-vim.keymap.set("n", "<leader>p", '"p+', { desc = "Paste from clipboard" })
+vim.keymap.set("v", "<leader>cc", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
