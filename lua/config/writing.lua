999589dd1c058876b8a4b0893383b30131819611
jmbeach <jaredbeachdesign@gmail.com>
2024-04-05 17:28:33 -0400

    writing

--- lua/config/writing.lua
+++ lua/config/writing.lua
@@ -2,3 +2,23 @@
 vim.api.nvim_create_user_command("Zen", function()
   vim.api.nvim_command("Goyo")
 end, {})
+
+vim.g.limelight_conceal_ctermfg = "gray"
+vim.g.limelight_conceal_ctermbg = 240
+vim.g.limelight_conceal_guifg = "DarkGray"
+vim.g.limelight_conceal_guifg = "#777777"
+vim.api.nvim_create_autocmd("User", {
+  pattern = "GoyoEnter",
+  callback = function()
+    vim.cmd("Limelight")
+    require("lualine").hide()
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "GoyoLeave",
+  callback = function()
+    vim.cmd("Limelight!")
+    require("lualine").hide({ unhide = true })
+  end,
+})
--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -2,20 +2,5 @@ return {
   { "junegunn/goyo.vim" },
   {
     "junegunn/limelight.vim",
-    config = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "GoyoEnter",
-        callback = function()
-          vim.cmd("Limelight")
-        end,
-      })
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "GoyoLeave",
-        callback = function()
-          vim.cmd("Limelight!")
-        end,
-      })
-    end,
   },
 }
