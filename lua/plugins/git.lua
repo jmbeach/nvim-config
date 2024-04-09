d5b01ad5c055628955862b1f074bff0feb41b135
jared.beach <jared.beach@capitalone.com>
2024-04-09 09:42:03 -0400

    move neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,15 @@
 return {
   { "f-person/git-blame.nvim" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+  },
 }
