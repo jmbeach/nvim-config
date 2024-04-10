c1c8bb37ec7b7c1ed853fa778e2013d6bf993a0f
jared.beach <jared.beach@capitalone.com>
2024-04-10 10:04:31 -0400

    Diffview: Close with q in normal mode

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -30,4 +30,17 @@ return {
       return opts
     end,
   },
+  {
+    "sindrets/diffview.nvim",
+    opts = function(_, opts)
+      opts.keymaps = {
+        view = {
+          { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+        },
+        file_panel = {
+          { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+        },
+      }
+    end,
+  },
 }
