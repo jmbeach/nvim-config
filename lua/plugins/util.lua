255ba4e587f290ddb22f020cdda931578fa68832
jared.beach <jared.beach@capitalone.com>
2024-04-11 11:57:10 -0400

    tmux navigator: adds tmux navigator

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,3 +1,21 @@
 return {
   { "mbbill/undotree" },
+  -- easily navigate between panes in tmux using c-h c-j c-k c-l
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
