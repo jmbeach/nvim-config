974033fb504c242685da7390d08d73dead86f25f
jared.beach <jared.beach@capitalone.com>
2024-04-15 18:24:08 -0400

    Copilot: Use <c-c> instead of <c-y> to select

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -118,7 +118,7 @@ return {
   {
     "github/copilot.vim",
     config = function()
-      vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+      vim.keymap.set("i", "<C-c>", 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false,
       })
