e529e1d8a6316e274a77da27f086f642fef80e84
jared.beach <jared.beach@capitalone.com>
2024-04-17 08:34:15 -0400

    Telescope: search resume with <leader>sr

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -108,7 +108,7 @@ return {
       { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
       { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
       { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+      { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "Resume" },
       { "<leader>sw", Util.telescope("grep_string", { word_match = "-w" }), desc = "Word (root dir)" },
       { "<leader>sW", Util.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" },
       { "<leader>/", Util.telescope("grep_string"), mode = "v", desc = "Find Selection in Files (root dir)" },
@@ -188,6 +188,7 @@ return {
               ["q"] = actions.close,
             },
           },
+          path_display = { "smart" },
         },
         pickers = {
           buffers = {
