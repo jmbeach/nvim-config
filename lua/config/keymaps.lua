6f9b3f009544bce8211f6badef737d8d24fc5650
jared.beach <jared.beach@capitalone.com>
2024-04-19 15:30:43 -0400

    Keymaps: telescope resume with <leader>sr

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,47 +6,52 @@ local neogit = require("neogit")
 local scroll = require("neoscroll")
 local util_vim_buffer = require("util.vim.buffer")
 
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local is_tmux = os.getenv("TMUX") ~= nil
+
+local map = vim.keymap.set
+
 -- Smooth scrolling on page up and down
 local scroll_down = function()
+  ---@diagnostic disable-next-line: missing-parameter
   scroll.scroll(vim.wo.scroll, true, 350)
 end
 local scroll_up = function()
+  ---@diagnostic disable-next-line: missing-parameter
   scroll.scroll(-vim.wo.scroll, true, 350)
 end
-vim.keymap.set("n", "<C-u>", scroll_up, { desc = "Half page up" })
-vim.keymap.set("n", "<C-d>", scroll_down, { desc = "Half page down" })
+
+map("n", "<C-u>", scroll_up, { desc = "Half page up" })
+map("n", "<C-d>", scroll_down, { desc = "Half page down" })
 
 -- Add a mapping to make alt + / open and hide the terminal
 local Util = require("lazyvim.util")
 local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root() })
 end
-vim.keymap.set("n", "<M-/>", lazyterm, { desc = "Terminal (root dir)" })
-vim.keymap.set("t", "<M-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-
-local is_mac = vim.loop.os_uname().sysname == "Darwin"
-local is_tmux = os.getenv("TMUX") ~= nil
+map("n", "<M-/>", lazyterm, { desc = "Terminal (root dir)" })
+map("t", "<M-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
 -- Fix some screwy stuff on mac in tmux
 if is_mac and is_tmux then
   -- Ctrl + - seems to be mapped to ctrl + _
   -- Overriding that to go back in jump list instead of opening terminal
-  vim.keymap.set("n", "<C-_>", "<C-o>", { desc = "Go back in jump list" })
+  map("n", "<C-_>", "<C-o>", { desc = "Go back in jump list" })
   -- might be an alacritty thing. Ctrl + a is just getting interpretted as a
-  vim.keymap.set("n", "+", "<c-a>", { desc = "Increment number" })
-  vim.keymap.set("v", "g+", "g<c-a>", { desc = "Increment numbers" })
+  map("n", "+", "<c-a>", { desc = "Increment number" })
+  map("v", "g+", "g<c-a>", { desc = "Increment numbers" })
 end
 
 -- Map leader f p to print full file path
-vim.keymap.set("n", "<leader>fp", ":lua print(vim.api.nvim_buf_get_name(0))<cr>", { desc = "Print full file path" })
+map("n", "<leader>fp", ":lua print(vim.api.nvim_buf_get_name(0))<cr>", { desc = "Print full file path" })
 
 -- copy / paste to clipboard
-vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set("n", "<leader>y", '"+yy', { desc = "Copy line to clipboard" })
-vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
+map("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+map("n", "<leader>y", '"+yy', { desc = "Copy line to clipboard" })
+map("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
 
 -- toggle completion
-vim.keymap.set(
+map(
   "n",
   "<leader>ct",
   ":lua require('cmp').setup { enabled = not require('cmp').config.enabled }<CR>",
@@ -54,10 +59,10 @@ vim.keymap.set(
 )
 
 -- make multiline string searchable
-vim.keymap.set("n", "<leader>sx", ":%s/\\n/\\\\n/g<cr>:%s/\\s\\s\\+/\\\\s\\\\+/g<cr>", { desc = "Make searchable" })
+map("n", "<leader>sx", ":%s/\\n/\\\\n/g<cr>:%s/\\s\\s\\+/\\\\s\\\\+/g<cr>", { desc = "Make searchable" })
 
 -- move word when broken up by key like "-"
-vim.keymap.set("n", "<M-w>", "/[_A-Z-]\\|\\><cr>", { desc = "Move word with seperators" })
+map("n", "<M-w>", "/[_A-Z-]\\|\\><cr>", { desc = "Move word with seperators" })
 
 -- use leader + g + d to open/close diff view
 local diffview_toggle = function()
@@ -71,24 +76,26 @@ local diffview_toggle = function()
     vim.cmd.DiffviewOpen()
   end
 end
-vim.keymap.set("n", "<leader>gd", diffview_toggle, { desc = "Diffview Open" })
+map("n", "<leader>gd", diffview_toggle, { desc = "Diffview Open" })
 
 -- use leader g + b to toggle git blame
-vim.keymap.set("n", "<leader>gb", "<Cmd>GitBlameToggle<CR>", { desc = "Git Blame Toggle" })
+map("n", "<leader>gb", "<Cmd>GitBlameToggle<CR>", { desc = "Git Blame Toggle" })
 
 -- use neogit instead of lazygit
-vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Neogit (root dir)" })
+map("n", "<leader>gg", neogit.open, { desc = "Neogit (root dir)" })
 
 -- general
-vim.keymap.set("v", "/", util_vim_buffer.search_visually_selected_text, { desc = "Visual search" })
-vim.keymap.set("n", "<leader>xm", "<cmd>messages<cr>", { desc = "Messages" })
+map("v", "/", util_vim_buffer.search_visually_selected_text, { desc = "Visual search" })
+map("n", "<leader>xm", "<cmd>messages<cr>", { desc = "Messages" })
 
-vim.keymap.set("n", "<leader>sr", "<cmd>Telescope resume<cr>", { desc = "Resume" })
-vim.keymap.set("n", "<leader>sR", function()
+-- spectre
+map("n", "<leader>sR", function()
   require("spectre").open()
 end, { desc = "Replace in Files (Spectre)" })
 
-vim.keymap.set(
+-- telescope
+map("n", "<leader>sr", "<cmd>Telescope resume<cr>", { desc = "Resume" })
+map(
   "n",
   "<leader>sC",
   ":lua require('telescope').extensions.neoclip.default()<cr>",
@@ -96,4 +103,4 @@ vim.keymap.set(
 )
 
 -- close all other buffers with <leader>bX
-vim.keymap.set("n", "<leader>bX", util_vim_buffer.close_all_but_open_buffer, { desc = "Close all but open" })
+map("n", "<leader>bX", util_vim_buffer.close_all_but_open_buffer, { desc = "Close all but open" })
