c7066f977d7e1205b9f88c1823573d34561e0b99
jmbeach <jaredbeachdesign@gmail.com>
2024-06-16 13:12:50 -0400

    Let j and k move virtual lines in text files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,6 +9,8 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
     vim.opt_local.linebreak = true
+    vim.keymap.set('n', 'j', 'gj', { buffer = true })
+    vim.keymap.set('n', 'k', 'gk', { buffer = true })
   end,
 })
 
