f57570c00aedbb1f2f7dc7c5853ad1f55afac470
jared.beach <jared.beach@capitalone.com>
2024-08-05 10:37:58 -0400

    copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,10 +1,35 @@
 return {
-  'github/copilot.vim',
-  config = function()
-    vim.keymap.set('i', '<C-c>', 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
-    vim.g.copilot_no_tab_map = true
-  end,
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.keymap.set('i', '<C-c>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    keys = {
+      {
+        '<leader>tc',
+        function()
+          require('CopilotChat').toggle()
+        end,
+        mode = 'n',
+        desc = '[T]oggle Copilot Chat',
+      },
+    },
+  },
 }
