fea39d8786104b52cab02cade25b5332d909e608
jared.beach <jared.beach@capitalone.com>
2024-12-13 11:04:27 -0500

    fix log files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,10 +42,20 @@ local fileTypes = {
 
 for extension, filetype in pairs(fileTypes) do
   vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile', 'FileType' }, {
-    pattern = { extension },
+    pattern = extension,
     group = augroup 'filetypes',
     callback = function()
-      vim.cmd.setfiletype(filetype)
+      vim.bo.filetype = filetype
     end,
   })
 end
+
+-- Rest stuff
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'http',
+  group = augroup 'rest',
+  callback = function()
+    vim.keymap.set('n', '<leader>cr', '<cmd>Rest run<cr>', { desc = 'Rest Run' })
+    vim.keymap.set('n', '<leader>ce', '<cmd>Rest env select<cr>', { desc = 'Rest env select' })
+  end,
+})
