273dfbd027949d28ee8dadca06d28ff6a9177f68
jared.beach <jared.beach@capitalone.com>
2025-05-13 12:38:56 -0400

    Enhance Copilot Chat with save, reset, and history features
    - Added `copilot_chat_save` to save chat sessions with a timestamped
      filename based on the prompt.
    - Introduced `copilot_chat_reset` to save and reset the chat session.
    - Implemented `copliot_chat_history` using Telescope to browse and
      load previous chat sessions.
    - Updated key mappings for reset and history functionalities.
    - Configured an autocmd to save chat sessions on buffer leave.
    - Added prompts for commit message generation with and without
      commitizen convention.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,39 @@
 local copilot_enabled = true
+local function copilot_chat_save()
+  -- Save to a file with today's date and first 50 characters of the prompt
+  local date = os.date '%Y-%m-%d'
+  local prompt = table.concat(vim.fn.getline(2, '$'), ' ')
+  local trimmed = prompt:match('^%s*(.-)%s*$'):sub(1, 50)
+  local sanitized_prompt = trimmed:gsub('[^%w%s]', '_'):gsub('%s+', '_')
+  if sanitized_prompt == '' then
+    return
+  end
+  local filename = string.format('copilot_%s_%s', date, sanitized_prompt)
+  vim.cmd('CopilotChatSave ' .. filename)
+end
+local function copilot_chat_reset()
+  copilot_chat_save()
+  vim.cmd 'CopilotChatReset'
+end
+local function copliot_chat_history()
+  local telescope = require 'telescope.builtin'
+  local function copilot_history_on_select(prompt_bufnr)
+    local selection = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+    require('telescope.actions').close(prompt_bufnr)
+    vim.cmd 'CopilotChatOpen'
+    vim.cmd('CopilotChatLoad ' .. selection.value:gsub('%.json$', ''))
+    return true
+  end
+  telescope.find_files {
+    prompt_title = 'Copilot Chat History',
+    cwd = vim.fn.stdpath 'data' .. '/copilotchat_history',
+    attach_mappings = function(_, map)
+      map({ 'i', 'n' }, '<CR>', copilot_history_on_select)
+      map({ 'i', 'n' }, '<C-y>', copilot_history_on_select)
+      return true
+    end,
+  }
+end
 return {
   {
     'github/copilot.vim',
@@ -40,7 +75,23 @@ return {
       mappings = {
         close = {
           normal = 'q',
+          -- do not quit with <C-c>
           insert = '<C-S-c>',
+        },
+        reset = {
+          -- do not reset with <C-l>
+          normal = '<C-S-r>',
+          insert = '<C-S-r>',
+        },
+      },
+      prompts = {
+        Commit = {
+          prompt = 'Write commit message for the change. Do not use commitizen convention; just use a general commit style. Keep the title under 50 characters and wrap message at 72 characters. Format as a gitcommit code block.',
+          context = 'git:staged',
+        },
+        CommitCommitizen = {
+          prompt = 'Write commit message for the change with commitizen convention. Keep the title under 50 characters and wrap message at 72 characters. Format as a gitcommit code block.',
+          context = 'git:staged',
         },
       },
     },
@@ -55,5 +106,17 @@ return {
         desc = '[T]oggle Copilot Chat',
       },
     },
+    config = function(_, opts)
+      require('CopilotChat').setup(opts)
+      vim.api.nvim_create_autocmd('BufLeave', {
+        pattern = 'copilot-*',
+        callback = function()
+          copilot_chat_save()
+        end,
+      })
+      vim.keymap.set('n', '<Leader>Cr', copilot_chat_reset, { desc = '[C]opilot [r]eset' })
+      vim.keymap.set('n', '<Leader>Ch', copliot_chat_history, { desc = '[C]opilot [h]istory' })
+      vim.keymap.set('n', '<Leader>Cc', '<cmd>CopilotChatCommit<CR>', { desc = '[C]opilot [c]commit' })
+    end,
   },
 }
