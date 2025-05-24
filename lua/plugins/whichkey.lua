041451be3cd86ec8bf66f351685c18f5e20effc4
jared.beach <jared.beach@capitalone.com>
2025-05-24 07:21:16 -0700

    feat(copilot): add commitizen support and update keybinds
    - Added a new keybinding '<Leader>CC' for CopilotChatCommitCommitizen.
    - Updated which-key groups to include '[C]opilot' for better organization.
    - Renamed '[R]ename' group to '[R]un' for improved clarity.

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -7,10 +7,11 @@ return { -- Useful plugin to show you pending keybinds.
     wk.add { '<leader><Tab>', group = '[<Tab>]' }
     wk.add { '<leader>b', group = '[B]uffer' }
     wk.add { '<leader>c', group = '[C]ode' }
+    wk.add { '<leader>C', group = '[C]opilot' }
     wk.add { '<leader>d', group = '[D]ocument' }
     wk.add { '<leader>f', group = '[F]ile' }
     wk.add { '<leader>g', group = '[G]it' }
-    wk.add { '<leader>r', group = '[R]ename' }
+    wk.add { '<leader>r', group = '[R]un' }
     wk.add { '<leader>s', group = '[S]earch' }
     wk.add { '<leader>t', group = '[T]oggle' }
     wk.add { '<leader>u', group = '[U]i' }
