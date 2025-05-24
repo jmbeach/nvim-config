0adf00725257908eb4429788f1356d9911778a83
jared.beach <jared.beach@capitalone.com>
2025-05-24 07:24:12 -0700

    feat(lsp): enhance Lua LSP setup with custom on_init
    - Added an `on_init` function to dynamically configure Lua LSP settings.
    - Checks for `.luarc.json` or `.luarc.jsonc` in the workspace folder.
    - Configures runtime version as 'LuaJIT' and updates workspace library.
    - Disables third-party checks for improved performance.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -221,14 +221,34 @@ return {
         -- cmd = { ... },
         -- filetypes = { ... },
         -- capabilities = {},
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
-          },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              },
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            },
+          })
+        end,
+        settings = {
+          Lua = {},
         },
       },
     }
