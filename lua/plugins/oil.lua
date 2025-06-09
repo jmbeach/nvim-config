599e423b58c0eb01c5c792703a8e14505f0bd5ce
jmbeach <jaredbeachdesign@gmail.com>
2025-06-08 20:04:32 -0400

    feat(oil): add keybinding for moving to right window
    Added a new keybinding `<C-l>` in the oil plugin configuration to allow
    users to move to the right window using `wincmd l`. This enhances
    navigation within the editor.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -28,6 +28,9 @@ return {
         ['<C-t>'] = 'actions.select_tab',
         ['<C-p>'] = 'actions.preview',
         ['<C-c>'] = 'actions.close',
+        ['<C-l>'] = function()
+          vim.cmd 'wincmd l'
+        end,
         ['-'] = 'actions.parent',
         ['_'] = 'actions.open_cwd',
         ['`'] = 'actions.cd',
