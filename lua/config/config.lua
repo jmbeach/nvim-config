7f64525ff28a88afe6d7eca9bf5e18edc296e6ff
jmbeach <jaredbeachdesign@gmail.com>
2025-07-04 10:58:38 -0400

    Make Goyo wider

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -1,6 +1,6 @@
 -- I don't want to remember the word "Goyo". "Zen" mode is what I actually want
 vim.api.nvim_create_user_command('Zen', function()
-  vim.api.nvim_command 'Goyo'
+  vim.api.nvim_command 'Goyo 120'
   vim.api.nvim_command 'GitBlameDisable'
 end, {})
 
