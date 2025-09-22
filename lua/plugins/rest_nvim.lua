5868d593cf58893bf8063fd673bad482f0a60089
jmbeach <jaredbeachdesign@gmail.com>
2025-09-22 19:39:50 -0400

    Makes rest.nvim json output formatted

--- lua/plugins/rest_nvim.lua
+++ lua/plugins/rest_nvim.lua
@@ -1,5 +1,14 @@
 return {
   {
     'rest-nvim/rest.nvim',
+    config = function()
+      -- This makes rest.nvim json formatted
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'json',
+        callback = function(ev)
+          vim.bo[ev.buf].formatprg = 'jq --indent 4'
+        end,
+      })
+    end,
   },
 }
