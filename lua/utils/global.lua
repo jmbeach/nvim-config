d6b392574960e7f86d2408eeb096ddeaad033bae
jmbeach <jaredbeachdesign@gmail.com>
2025-09-22 20:16:50 -0400

    String split function

--- lua/utils/global.lua
+++ lua/utils/global.lua
@@ -5,3 +5,14 @@ end
 function StringStartsWith(str, start)
   return str:sub(1, #start) == start
 end
+
+function StringSplit(inputstr, sep)
+  if sep == nil then
+    sep = '%s+' -- Default to splitting by one or more whitespace characters
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, '([^' .. sep .. ']+)') do
+    table.insert(t, str)
+  end
+  return t
+end
