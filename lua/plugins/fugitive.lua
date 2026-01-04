0d95d15d615b58f016ce9509871e77695c4619c1
Jared Beach <jaredbeachdesign@gmail.com>
2026-01-04 13:23:42 -0500

    Open new tab for commit message
    Makes fugitive commit open a new tab so that it's easier to see what's
    changed while writing your commit message

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -9,7 +9,22 @@ return {
       pattern = { 'gitcommit' },
       group = vim.api.nvim_create_augroup('my_gitcommit', { clear = true }),
       callback = function()
-        vim.cmd 'wincmd H'
+        -- 1. Open the commit buffer in a new tab
+        vim.cmd 'wincmd T'
+
+        -- 2. Create a vertical split (Copy of the buffer)
+        vim.cmd 'vsplit'
+
+        -- Search for the specific git instruction line and scroll it to the top
+        -- We search for the line starting with "# Everything below"
+        if vim.fn.search([[^# Everything below]], 'w') > 0 then
+          vim.cmd 'normal! zt'
+        end
+
+        -- Move focus back to the Left window (Writing window)
+        vim.cmd 'wincmd h'
+        local width = math.floor(vim.o.columns * 0.4)
+        vim.cmd('vertical resize ' .. width)
       end,
     })
   end,
